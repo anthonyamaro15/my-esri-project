ff0bd1960c0659715caf2889c61e4a34194fb671
Anthony Amaro <aamaro@bradmins-MacBook-Pro.local>
2021-03-25 13:51:39 -0700

    feat(map-controller): add input range
    added input range and filter data with those values as well, fixed bug that was preventing from dislaying the filtered data

--- src/components/mapview/control-panel.tsx
+++ src/components/mapview/control-panel.tsx
@@ -1,10 +1,36 @@
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import MapController from '../../controllers/mapController';
+import { mapLoaded as mapLoaderSelector } from '../../store/slices/mapSlice';
+
 const ControlPanel = () => {
+    const [updateBrigntness, setUpdateBrightness] = useState<number>(0);
+    const mapLoaded = useSelector(mapLoaderSelector);
+
+    useEffect(() => {
+        if(mapLoaded) {
+            MapController.updateBrightness(updateBrigntness);
+        // console.log("inside ", updateBrigntness);
+
+        }
+        // console.log("outside ", updateBrigntness);
+    },[updateBrigntness, mapLoaded]);
+
+    
+
     return (
-       <div className="esri-widget" id="brightness-filter">
-           <div className="brightness" data-bright='300'>{'> 300'}</div>
-           <div className="brightness" data-bright='280'>{'> 280'}</div>
-           <div className="brightness" data-bright='200'>{'< 200'}</div>
-       </div> 
+        <div>
+            <div className="esri-widget" id="brightness-filter">
+                <div className="brightness" onClick={() => setUpdateBrightness(300)} data-bright='300'>{'> 300'}</div>
+                <div className="brightness" data-bright='280' onClick={() => setUpdateBrightness(280)}>{'> 280'}</div>
+                <div className="brightness" data-bright='200' onClick={() => setUpdateBrightness(200)}>{'< 200'}</div>
+                <div className="control-brightness">
+                    <input type="range" min="0" max="400" value={updateBrigntness} onChange={(e) => setUpdateBrightness(Number(e.target.value))}/>
+            </div>
+            </div> 
+
+        </div>
+
     )
 }
 
